ead3bbb5ece047a57d493c7258b57263414a7e81
yulian-khalitov <khalitov.yulian@yandex.ru>
2021-11-06 20:44:43 +0300

    start docker before pull

--- stage2/04-ducatusx/00-run.sh
+++ stage2/04-ducatusx/00-run.sh
@@ -4,5 +4,6 @@ jinja2 files/config.toml.j2 -D node_eth_address="${NODE_ETH_ADDRESS}" -o config.
 install -m 644 config.toml "${ROOTFS_DIR}/var/"
 
 on_chroot << EOF
+systemctl start docker
 docker pull rocknblock/parity-aarch64:latest
 EOF
