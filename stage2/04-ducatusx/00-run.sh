15932c6e14291fbc99e68feee37fed40a1aa4e31
yulian-khalitov <khalitov.yulian@yandex.ru>
2021-11-06 19:44:31 +0300

    fix config name

--- stage2/04-ducatusx/00-run.sh
+++ stage2/04-ducatusx/00-run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
-jinja2 files/config.testnet.toml.j2 -D node_eth_address="${NODE_ETH_ADDRESS}" -o config.toml
+jinja2 files/config.toml.j2 -D node_eth_address="${NODE_ETH_ADDRESS}" -o config.toml
 install -m 644 config.toml "${ROOTFS_DIR}/var/"
