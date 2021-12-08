7175b783d6f15a041ba5c48bf489fa2fee766ecf
yulian-khalitov <khalitov.yulian@yandex.ru>
2021-12-08 18:22:33 +0300

    fix build

--- build.sh
+++ build.sh
@@ -312,10 +312,9 @@ for (( j = 0; j < "${#PARITY_PRIVATE_KEYS_ARRAY[@]}"; ++j )); do
 	PARITY_PRIVATE_KEY="${PARITY_PRIVATE_KEYS_ARRAY[$j]}"
 	STAGE_DIR=$(realpath "private-key")
 	run_stage
-
+	EXPORT_DIR="${STAGE_DIR}"
 	ZIP_FILENAME="parity-${PARITY_PRIVATE_KEY_NUM}"
 	STAGE_DIR="${BASE_DIR}/export-image"
-	EXPORT_DIR="${STAGE_DIR}"
 	EXPORT_ROOTFS_DIR=${WORK_DIR}/$(basename "${EXPORT_DIR}")/rootfs
 	run_stage
 done
