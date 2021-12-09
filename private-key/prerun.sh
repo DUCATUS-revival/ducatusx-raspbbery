d2c71c98451f5cf3410978d67b5195ffb56a9c10
yulian-khalitov <khalitov.yulian@yandex.ru>
2021-12-09 12:11:54 +0300

    fix

--- private-key/prerun.sh
+++ private-key/prerun.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 if [ ! -d "${ROOTFS_DIR}" ]; then
-    STAGE2_ROOTFS_DIR = "${WORK_DIR}/stage2/rootfs"
+    STAGE2_ROOTFS_DIR="${WORK_DIR}/stage2/rootfs"
 	if [ ! -d "${STAGE2_ROOTFS_DIR}" ]; then
 		echo "Stage2 rootfs not found"
 		false
