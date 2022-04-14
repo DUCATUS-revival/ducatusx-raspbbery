07fce2ba83b70e22c111dc6949547e7ae5e12520
yulian-khalitov <khalitov.yulian@yandex.ru>
2022-04-14 19:03:04 +0300

    update run script

--- stage2/02-wifi-connect/01-run.sh
+++ stage2/02-wifi-connect/01-run.sh
@@ -1,5 +1,10 @@
 #!/bin/bash -e
 
+mkdir -p "${ROOTFS_DIR}/etc/wifi-connect"
+install -m 700 scripts/start.sh "${ROOTFS_DIR}/etc/wifi-connect/"
+install -m 644 files/wifi-connect.service "${ROOTFS_DIR}/etc/systemd/system/"
+
+on_chroot << EOF
 install_wfc() {
     local _wfc_repo='balena-os/wifi-connect'
     local _wfc_install_root='/usr/local'
@@ -47,11 +52,6 @@ ensure() {
     fi
 }
 
-mkdir -p "${ROOTFS_DIR}/etc/wifi-connect"
-install -m 700 scripts/start.sh "${ROOTFS_DIR}/etc/wifi-connect/"
-install -m 644 files/wifi-connect.service "${ROOTFS_DIR}/etc/systemd/system/"
-
-on_chroot << EOF
 install_wfc
 systemctl enable wifi-connect
 EOF
