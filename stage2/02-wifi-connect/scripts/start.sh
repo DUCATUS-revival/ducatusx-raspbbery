23d3a417e04dc1ce867688f914af656747cd8a46
yulian-khalitov <khalitov.yulian@yandex.ru>
2022-04-15 12:56:08 +0300

    update start script

--- stage2/02-wifi-connect/scripts/start.sh
+++ stage2/02-wifi-connect/scripts/start.sh
@@ -2,21 +2,26 @@
 
 sleep 30
 
-# 1. Is there a default gateway?
-# ip route | grep default
+while true
+do
+    # 1. Is there a default gateway?
+    # ip route | grep default
 
-# 2. Is there Internet connectivity?
-nmcli -t g | grep full
+    # 2. Is there Internet connectivity?
+    nmcli -t g | grep full
 
-# 3. Is there Internet connectivity via a google ping?
-wget --spider http://google.com 2>&1
+    # 3. Is there Internet connectivity via a google ping?
+    wget --spider http://google.com 2>&1
 
-# 4. Is there an active WiFi connection?
-iwgetid -r
+    # 4. Is there an active WiFi connection?
+    iwgetid -r
 
-if [ $? -eq 0 ]; then
-    printf 'Skipping WiFi Connect\n'
-else
-    printf 'Starting WiFi Connect\n'
-    wifi-connect --portal-ssid DucatusX-RPi
-fi
+    if [ $? -eq 0 ]; then
+        printf 'Skipping WiFi Connect\n'
+    else
+        printf 'Starting WiFi Connect\n'
+        wifi-connect --portal-ssid DucatusX-RPi
+    fi
+
+    sleep 60
+done
